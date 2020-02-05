20d2f1fb5cc9494933e0bfdac12b4832c9573ac2
lukemiller93 <lukem1293@gmail.com>
2020-02-05 08:37:43 -0600

    restructured existing types and added some new ones

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -2,9 +2,12 @@
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
 import createSchema from "part:@sanity/base/schema-creator";
-import blogPost from "./blogPost";
-import sponsor from "./sponsor";
-
+import blogPost from "./documents/blogPost";
+import person from "./documents/person";
+import siteSettings from "./documents/siteSettings";
+import bioPortableText from "./objects/bioPortableText";
+import figure from "./objects/figure";
+import sponsor from "./objects/sponsor";
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -12,7 +15,18 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    /**
+     * When added to this list, object types can be used as
+     * {type: 'typename'} in other document types
+     */
+    bioPortableText,
+    figure,
+    /**
+     * The following types are document types which will appear
+     * in the studio
+     */
+    person,
+    siteSettings,
     blogPost,
     sponsor
   ])
